e2c7238e764aeb8767969eda6bfe3bc49f13aa92
MindfulLearner <dimajosh1009@gmail.com>
2024-08-27 01:07:03 +0200

    end

--- main.js
+++ main.js
@@ -89,35 +89,41 @@ let postTime = document.getElementsByClassName("post-meta__time");
 
 for (let i = 0; i < posts.length; i++) {
   postList.innerHTML += `
-    <div class="post">
+        <div class="post">
         <div class="post__header">
-            <div class="post-meta">
-                <div class="post-meta__icon">
-                    <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">
-                </div>
-                <div class="post-meta__data">
+        <div class="post-meta">
+        <div class="post-meta__icon">
+        <img class="profile-pic" src="${posts[i]["author"]["image"]}" alt="Phil Mangione">
+        </div>
+        <div class="post-meta__data">
         <div class="post-meta__author">${posts[i]["author"]["name"]}</div>
-                    <div class="post-meta__time">4 mesi fa</div>
-                </div>
-            </div>
+        <div class="post-meta__time">${posts[i]["created"]}</div>
+        </div>
+        </div>
         </div>
         <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
         <div class="post__image">
         <img class="post__image_img" src="${posts[i]["media"]}" alt="">
         </div >
         <div class="post__footer">
-            <div class="likes js-likes">
-                <div class="likes__cta">
+        <div class="likes js-likes">
+        <div class="likes__cta">
         <a class="like-button  js-like-button" href="#" data-postid="${posts[i]["id"]}">
-                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                        <span class="like-button__label">Mi Piace</span>
-                    </a>
-                </div>
-                <div class="likes__counter">
-                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-                </div>
-            </div>
+        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+        <span class="like-button__label">Mi Piace</span>
+        </a>
         </div>
-    </div >
+        <div class="likes__counter">
+        Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i]["likes"]}</b> persone
+        </div>
+        </div>
+        </div>
+        </div >
         `;
+
+  let likeButtons = document.getElementsByClassName("like-button");
+  likeButtons[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("hai stampato " + i);
+  });
 }
